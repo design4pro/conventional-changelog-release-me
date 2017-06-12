f71c36f7419ed8ea21ed0bb0936821b9e0738216
Rafał Wolak <rafal@tomandco.co.uk>
2017-06-12 12:35:38 +0200

    feat(package): remove babel, update dependencies and add release-me

--- index.js
+++ index.js
@@ -8,42 +8,41 @@ const resolve = require('path').resolve;
 const changelogrcConfig = require('./lib/changelogrc-config');
 const transformFn = require('./lib/transform-fn');
 
-module.exports = Promise.all([
-  changelogrcConfig()
-]).then(args => Q.all([
+let config = changelogrcConfig();
+
+module.exports = Promise.resolve(config).then(value => Q.all([
   readFile(resolve(__dirname, 'templates/template.hbs'), 'utf-8'),
   readFile(resolve(__dirname, 'templates/header.hbs'), 'utf-8'),
   readFile(resolve(__dirname, 'templates/commit.hbs'), 'utf-8'),
   readFile(resolve(__dirname, 'templates/footer.hbs'), 'utf-8')
-])
-  .spread((template, header, commit, footer) => {
-    const parserOpts = {
-      headerPattern: /^(\w*)(?:\((.*)\))?\: (.*)$/,
-      headerCorrespondence: [
-        'type',
-        'scope',
-        'subject'
-      ],
-      noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-      revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
-      revertCorrespondence: ['header', 'hash']
-    };
+]).spread((template, header, commit, footer) => {
+  const parserOpts = {
+    headerPattern: /^(\w*)(?:\((.*)\))?\: (.*)$/,
+    headerCorrespondence: [
+      'type',
+      'scope',
+      'subject'
+    ],
+    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+    revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
+    revertCorrespondence: ['header', 'hash']
+  };
 
-    let writerOpts = {
-      mainTemplate: template,
-      headerPartial: header,
-      commitPartial: commit,
-      footerPartial: footer,
-      transform: transformFn(args[0])
-    };
+  let writerOpts = {
+    mainTemplate: template,
+    headerPartial: header,
+    commitPartial: commit,
+    footerPartial: footer,
+    transform: transformFn(value)
+  };
 
-    return {
-      parserOpts: parserOpts,
-      writerOpts: writerOpts,
-      groupBy: 'type',
-      commitGroupsSort: compareFunc(['position', 'title']),
-      commitsSort: compareFunc(['scope', 'subject']),
-      noteGroupsSort: 'title',
-      notesSort: compareFunc
-    };
-  }));
+  return {
+    parserOpts: parserOpts,
+    writerOpts: writerOpts,
+    groupBy: 'type',
+    commitGroupsSort: compareFunc(['position', 'title']),
+    commitsSort: compareFunc(['scope', 'subject']),
+    noteGroupsSort: 'title',
+    notesSort: compareFunc
+  };
+}));
